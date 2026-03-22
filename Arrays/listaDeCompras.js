4e227faf0f029cb21f5012652c6dd282b511f111
Ana R. Carvalho <anarcarvalho93@gmail.com>
2026-03-22 17:26:21 -0300

    .

---
+++ Arrays/listaDeCompras.js
@@ -0,0 +1,20 @@
+/* LISTA DE COMPRAS -> ARRAYS [VETORES] */
+
+const ListaDeCompras = []
+console.log(ListaDeCompras)
+
+ListaDeCompras [0] = "Arroz"
+ListaDeCompras [1] = "Feijão"
+ListaDeCompras [2] = 7
+
+console.log(ListaDeCompras)
+
+// EXEMPLO DE LISTA DE COMPRAS 2 //
+
+const ListaDeCompras = ["Açucar" , false]
+
+ListaDeCompras [0] = "Arroz"
+ListaDeCompras [1] = "Feijão"
+ListaDeCompras [2] = 8
+
+console.log(ListaDeCompras)
