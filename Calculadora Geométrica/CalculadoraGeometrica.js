a7e86ff90c175d929276cf9998ff990af43a6947
Ana R. Carvalho <anarcarvalho93@gmail.com>
2026-03-31 12:48:05 -0300

    Alteração no codigo JS

--- Calculadora Geométrica/CalculadoraGeometrica.js
+++ Calculadora Geométrica/CalculadoraGeometrica.js
@@ -42,3 +42,55 @@ function calcularAreaCirculo() {
 
 // Um programa que usa funções sempre que possível para separar os procedimentos. O programa também contém
 // A opção de “Sair” e enquanto ela não for escolhida deverá voltar ao menu principal.
+
+function exibirMenu() {
+  return prompt(
+    "Calculadora Geométrica\n" +
+    "1. Calcular área de triângulo\n" +
+    "2. Calcular área de retângulo\n" +
+    "3. Calcular área de quadrado\n" +
+    "4. Calcular área de trapézio\n" +
+    "5. Calcular área de círculo\n" +
+    "6. Sair\n"
+  )
+}
+
+function executar() {
+  let opcao = ""
+
+  do {
+    opcao = exibirMenu()
+    let resultado
+
+    switch (opcao) {
+      case "1":
+        resultado = calcularAreaTriangulo()
+        break
+      case "2":
+        resultado = calcularAreaRetangulo()
+        break
+      case "3":
+        resultado = calcularAreaQuadrado()
+        break
+      case "4":
+        resultado = calcularAreaTrapezio()
+        break
+      case "5":
+        resultado = calcularAreaCirculo()
+        break
+      case "6":
+        alert("Saindo...")
+        break
+      default:
+        alert("Opção inválida!")
+        break
+    }
+
+    if (resultado) {
+      alert("Resultado: " + resultado)
+    }
+
+  } while (opcao !== "6");
+}
+
+executar()
